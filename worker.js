b5efb47988108641e86d720a620ad98a8d62ae77
Artod <artem.belousov@autodesk.com>
2021-12-20 20:19:04 -0500

    Passing format of frames #8
    Pass frames from the creature's camera in both ways

--- worker.js
+++ worker.js
@@ -1,5 +1,7 @@
 (async () => {
     importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js");
+    importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js");
+    
                     
     // Define a model for linear regression.
     const model = tf.sequential();
@@ -17,7 +19,7 @@
     // Use the model to do inference on a data point the model hasn't seen before:
     // model.predict(tf.tensor2d([6], [1, 1])).print();
 
-    self.addEventListener('message', async e => {
+    self.addEventListener('message', async (e) => {
 
         // const ta = new Int32Array(e.data)
         // console.log(new Uint8Array(e.data))
@@ -27,8 +29,30 @@
 
         // const frame = tf.browser.fromPixels(blob)
 
-        // console.log(`Worker: `, e.data.shape())
-        // send a message back to the main thread
-        self.postMessage(e.data.shape)
+        // console.log(`Worker: `, e.data.shape)
+
+        const frame = tf.tensor3d(e.data, [75, 150, 3])
+
+        // const base64Data = e.data
+        // const DecodeBase64ToBinary = (base64Data) => {
+        //     const decodedString = atob(base64Data);
+        //     const bufferLength = decodedString.length;
+        //     const bufferView = new Uint8Array(new ArrayBuffer(bufferLength));
+
+        //     for (let i = 0; i < bufferLength; i++) {
+        //         bufferView[i] = decodedString.charCodeAt(i);
+        //     }
+
+        //     return bufferView;
+        // };
+
+        // const frame = tf.tensor3d(dsd, [100, 200, 3])
+        
+        // const frame = tf.node.decodeJpeg(DecodeBase64ToBinary(base64Data))
+        const data = await frame.array()
+        self.postMessage({
+            data,
+            console: "" 
+        })
     })
 })()
