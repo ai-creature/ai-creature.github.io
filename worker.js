15d89cd491220db503aa4ce3c068fb983820d893
Artod <artem.belousov@autodesk.com>
2021-12-24 18:15:05 -0500

    LSTM layers #15

--- worker.js
+++ worker.js
@@ -20,7 +20,11 @@ const strides = 1
 const padding = 'same'
 let filterPow = 3
 
-model.add(tf.layers.inputLayer({inputShape}))
+
+model.add(tf.layers.inputLayer({
+    inputShape, 
+    batchSize : 1
+}))
 
 layers = 14
 for (let i = 0; i < layers; i++) {
@@ -45,8 +49,37 @@ for (let i = 0; i < layers; i++) {
     }
 }
 
+// const reshapeLayer = tf.layers.convLstm2d()
+
+
+model.add(tf.layers.reshape({targetShape: [1, 512]}))
+
+model.add(tf.layers.dense({units: 256}))
+model.add(tf.layers.dense({units: 256}))
+model.add(tf.layers.dense({units: 256}))
+
+model.add(tf.layers.rnn({
+    cell: [
+        tf.layers.lstmCell({units: 128}),
+        tf.layers.lstmCell({units: 128}),
+        tf.layers.lstmCell({units: 128})
+    ], 
+    returnSequences: true,
+    returnState: false,
+    stateful: true
+}))
+
+model.add(tf.layers.dense({units: 64}))
+model.add(tf.layers.dense({units: 64}))
+model.add(tf.layers.dense({units: 64}))
+
+
+
 model.compile({optimizer: 'adam', loss: 'meanSquaredError'})
 console.log(model.summary())
+
+
+
 
 let busy = false
 self.addEventListener('message', async e => {
@@ -61,14 +94,19 @@ self.addEventListener('message', async e => {
 
     // console.log((await frameNorm.array())[0][0])
 
+
+
     if (true && !busy) {
         busy = true
 
+        console.log("getBackend: " + tf.getBackend ())
         let st = Date.now()
         // console.time("predict: " + st)
         const res = model.predict(frameNorm.expandDims(0))
         // res.print()
-        console.log("Pred: " + (await res.array())[0][0][0][0])
+        // console.log(res.shape)
+        // console.log("Pred: ", (await res.array())) //[0][0][0][0]
+        // return
         // console.timeEnd("predict: " + st)
 
 
@@ -77,7 +115,8 @@ self.addEventListener('message', async e => {
         // console.time("fit: " + st)
         const h = await model.fit(frameNorm.expandDims(0), tf.ones(res.shape), {
             batchSize: 1,
-            epochs: 1
+            epochs: 1,
+            shuffle: false
         });
         console.log("Loss: " + h.history.loss[0])
         // console.timeEnd("fit: " + st)
@@ -88,7 +127,7 @@ self.addEventListener('message', async e => {
 
     const data = await frame.array()
     // console.log(data[0][0])
-    
+
     frame.dispose()
     frameNorm.dispose()
 
