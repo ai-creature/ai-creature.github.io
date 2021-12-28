5dd99fc34c7a194bc45aa114d8907d46680e8bd7
Artod <artem.belousov@autodesk.com>
2021-12-27 21:26:33 -0500

    Functional layers #17

--- worker.js
+++ worker.js
@@ -12,73 +12,75 @@ model.predict(tf.tensor2d([6], [1, 1])).print();
 
 importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.12.0/dist/tf.min.js");
 
-const model = tf.sequential();
-const inputShape = [128, 256, 3]
+// const model = tf.sequential();
+const shape = [128, 256, 3]
+const batchSize = 1
 const kernelSize = [3, 3]
 const poolSize = 2
 const strides = 1
 const padding = 'same'
 let filterPow = 3
 
-
-model.add(tf.layers.inputLayer({
-    inputShape, 
-    batchSize : 1
-}))
+const inputs = tf.input({
+    // shape, 
+    batchShape : [batchSize, ...shape]
+})
 
 layers = 14
+let outputs = inputs
 for (let i = 0; i < layers; i++) {
     if (i%3 == 1) {
         filterPow++
         
     }
 
-    model.add(tf.layers.conv2d({
+    outputs = tf.layers.conv2d({
         filters: 2**filterPow,
         kernelSize,
-        activation: 'relu',
-        padding,
         strides,
+        padding,
+        activation: 'relu',
         kernelInitializer: 'heNormal'
-    }))
+    }).apply(outputs)
 
     if (i%2 == 1) {
-        model.add(tf.layers.maxPooling2d({
+        outputs = tf.layers.maxPooling2d({
             poolSize
-        }))
+        }).apply(outputs)
     }
 }
 
-// const reshapeLayer = tf.layers.convLstm2d()
+console.log(outputs.shape)
+outputs = tf.layers.reshape({targetShape: [1, 512]}).apply(outputs)
+// outputs = tf.layers.flatten().apply(outputs)
+console.log(outputs.shape)
+// outputs = tf.layers.dense({units: 256}).apply(outputs)
+// outputs = tf.layers.dense({units: 256}).apply(outputs)
+// outputs = tf.layers.dense({units: 256}).apply(outputs)
+
+let rnnState = []
 
 
-model.add(tf.layers.reshape({targetShape: [1, 512]}))
-
-model.add(tf.layers.dense({units: 256}))
-model.add(tf.layers.dense({units: 256}))
-model.add(tf.layers.dense({units: 256}))
-
-model.add(tf.layers.rnn({
-    cell: [
-        tf.layers.lstmCell({units: 128}),
-        tf.layers.lstmCell({units: 128}),
-        tf.layers.lstmCell({units: 128})
-    ], 
+;[outputs, ...rnnState] = tf.layers.gru({
+// outputs = tf.layers.gru({
+    units: 8,
+    stateful: true,
     returnSequences: true,
-    returnState: false,
-    stateful: true
-}))
+    returnState: true
+}).apply(outputs)
 
-model.add(tf.layers.dense({units: 64}))
-model.add(tf.layers.dense({units: 64}))
-model.add(tf.layers.dense({units: 64}))
+console.log("RNN inner state:", rnnState.map(s => s.shape))
+
+// outputs = tf.layers.dense({units: 64}).apply(outputs)
+// outputs = tf.layers.dense({units: 64}).apply(outputs)
+// outputs = tf.layers.dense({units: 64}).apply(outputs)
 
 
+const model = tf.model({inputs, outputs})
 
-model.compile({optimizer: 'adam', loss: 'meanSquaredError'})
+model.compile({optimizer: 'rmsprop', loss: 'meanSquaredError'})
+
 console.log(model.summary())
-
-
 
 
 let busy = false
@@ -86,7 +88,7 @@ self.addEventListener('message', async e => {
     const meanRgb = [0.485, 0.456, 0.406]
     const stdRgb = [0.229, 0.224, 0.225]
 
-    const frame = tf.tensor3d(e.data, inputShape, 'float32')
+    const frame = tf.tensor3d(e.data, shape, 'float32')
     const frameNorm = frame
         .div(tf.scalar(255))
         .sub(meanRgb)
@@ -95,31 +97,26 @@ self.addEventListener('message', async e => {
     // console.log((await frameNorm.array())[0][0])
 
 
-
     if (true && !busy) {
         busy = true
 
-        console.log("getBackend: " + tf.getBackend ())
-        let st = Date.now()
-        // console.time("predict: " + st)
-        const res = model.predict(frameNorm.expandDims(0))
+        const res = model.predict(tf.stack([frameNorm]), {
+            batchSize
+        })
         // res.print()
-        // console.log(res.shape)
+        console.log("res.shape: ", res.shape)
         // console.log("Pred: ", (await res.array())) //[0][0][0][0]
-        // return
-        // console.timeEnd("predict: " + st)
 
+        const result = model.evaluate(tf.stack([frameNorm]), tf.ones(res.shape), {batchSize});
+        result.print();
 
-
-        // st = Date.now()
-        // console.time("fit: " + st)
-        const h = await model.fit(frameNorm.expandDims(0), tf.ones(res.shape), {
-            batchSize: 1,
+        const h = await model.fit(tf.stack([frameNorm]), tf.ones(res.shape), {
+            batchSize,
             epochs: 1,
             shuffle: false
-        });
+        })
+
         console.log("Loss: " + h.history.loss[0])
-        // console.timeEnd("fit: " + st)
 
         busy = false
     }
