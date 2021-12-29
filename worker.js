2ea71e22cf81afc7f3f205ebf6ede24507675fb2
Artod <artem.belousov@autodesk.com>
2021-12-29 00:44:58 -0500

    RNN #15
    - Bug in tf.js that doesn't allow to pass sequence with the length one into a RNN

--- worker.js
+++ worker.js
@@ -1,18 +1,5 @@
-/*
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-const xs = tf.tensor2d([1, 2, 3, 4, 5], [5, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7, 8], [5, 1]);
-
-await model.fit(xs, ys, {epochs: 100});
-model.predict(tf.tensor2d([6], [1, 1])).print(); 
-*/
-
 importScripts("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.12.0/dist/tf.min.js");
 
-// const model = tf.sequential();
 const shape = [128, 256, 3]
 const batchSize = 1
 const kernelSize = [3, 3]
@@ -26,12 +13,15 @@ const inputs = tf.input({
     batchShape : [batchSize, ...shape]
 })
 
+const inputsRnn = tf.input({ 
+    batchShape : [1, 8]
+})
+
 layers = 14
 let outputs = inputs
 for (let i = 0; i < layers; i++) {
     if (i%3 == 1) {
         filterPow++
-        
     }
 
     outputs = tf.layers.conv2d({
@@ -50,76 +40,113 @@ for (let i = 0; i < layers; i++) {
     }
 }
 
-console.log(outputs.shape)
-outputs = tf.layers.reshape({targetShape: [1, 512]}).apply(outputs)
-// outputs = tf.layers.flatten().apply(outputs)
-console.log(outputs.shape)
-// outputs = tf.layers.dense({units: 256}).apply(outputs)
-// outputs = tf.layers.dense({units: 256}).apply(outputs)
-// outputs = tf.layers.dense({units: 256}).apply(outputs)
+outputs = tf.layers.flatten().apply(outputs)
+
+// outputs = tf.layers.dense({units: 64}).apply(outputs)
 
 let rnnState = []
+// ;[outputs, ...rnnState]
+// outputs = tf.layers.dropout(0.1).apply(outputs)
 
 
-;[outputs, ...rnnState] = tf.layers.gru({
-// outputs = tf.layers.gru({
-    units: 8,
+outputs = tf.layers.repeatVector({n: 2}).apply(outputs)
+outputs = tf.layers.rnn({
+    cell:[
+        tf.layers.gruCell({units: 32}),
+        // tf.layers.gruCell({units: 16}),
+        // tf.layers.gruCell({units: 8}),
+    ],
+    // units: 8,
     stateful: true,
-    returnSequences: true,
-    returnState: true
+    returnSequences: false,
+    returnState: false
 }).apply(outputs)
 
-console.log("RNN inner state:", rnnState.map(s => s.shape))
-
-// outputs = tf.layers.dense({units: 64}).apply(outputs)
-// outputs = tf.layers.dense({units: 64}).apply(outputs)
-// outputs = tf.layers.dense({units: 64}).apply(outputs)
+// outputs = tf.layers.dense({units: 4}).apply(outputs)
 
 
-const model = tf.model({inputs, outputs})
+// console.log("RNN inner state:", rnnState.map(s => s.shape))
 
-model.compile({optimizer: 'rmsprop', loss: 'meanSquaredError'})
 
+const model = tf.model({inputs: inputs, outputs})
+model.compile({optimizer: 'adam', loss: 'meanSquaredError'})
 console.log(model.summary())
+// model.weights.forEach(w => {console.log(w.name, w.shape);});
 
 
 let busy = false
+let i = 0
+let prevIsBlack = false
 self.addEventListener('message', async e => {
+    if (true && busy) return
+
+    i++
+
     const meanRgb = [0.485, 0.456, 0.406]
     const stdRgb = [0.229, 0.224, 0.225]
 
+    const isBlack = Math.random() <= 0.3
+
+    const val = isBlack ? 0 : 255
+
+    e.data[0][0] = [val, val, val]
+    e.data[0][1] = [val, val, val]
+    e.data[0][2] = [val, val, val]
+
+    e.data[1][0] = [val, val, val]
+    e.data[1][1] = [val, val, val]
+    e.data[1][2] = [val, val, val]
+
+    e.data[2][0] = [val, val, val]
+    e.data[2][1] = [val, val, val]
+    e.data[2][2] = [val, val, val]
+
     const frame = tf.tensor3d(e.data, shape, 'float32')
+
     const frameNorm = frame
         .div(tf.scalar(255))
-        .sub(meanRgb)
-        .div(stdRgb)
-
+        // .sub(meanRgb)
+        // .div(stdRgb)
     // console.log((await frameNorm.array())[0][0])
 
+    busy = true
+i%11 && model.resetStates()
+    const input = tf.stack([frameNorm])
+    const res = model.predict(input, {batchSize})
+    const labels = prevIsBlack ? tf.ones(res.shape) : tf.zeros(res.shape)
+    const h = await model.fit(input, labels, {
+        batchSize,
+        epochs: 1,
+        shuffle: false,
+        verbose: 2
+    })
 
-    if (true && !busy) {
-        busy = true
+    prevIsBlack = isBlack
+    
+    console.log("Losss: " + h.history.loss[0])
 
-        const res = model.predict(tf.stack([frameNorm]), {
-            batchSize
+
+    input.dispose()
+    res.dispose()
+    labels.dispose()
+
+    
+
+    /*const optimizer = tf.train.rmsprop(0.01)
+    const labels = tf.ones(res.shape)
+    optimizer.minimize(() => {
+        const pred = model.predict(input, {batchSize})
+        const loss = tf.losses.meanSquaredError(labels, pred);
+        
+        loss.data().then(h => {
+            busy = false
+            console.log('Loss: ', h[0])
         })
-        // res.print()
-        console.log("res.shape: ", res.shape)
-        // console.log("Pred: ", (await res.array())) //[0][0][0][0]
-
-        const result = model.evaluate(tf.stack([frameNorm]), tf.ones(res.shape), {batchSize});
-        result.print();
-
-        const h = await model.fit(tf.stack([frameNorm]), tf.ones(res.shape), {
-            batchSize,
-            epochs: 1,
-            shuffle: false
-        })
-
-        console.log("Loss: " + h.history.loss[0])
-
-        busy = false
-    }
+        
+        return loss;
+    })*/
+    
+    // else console.log("skip")
 
 
     const data = await frame.array()
@@ -129,5 +156,7 @@ self.addEventListener('message', async e => {
     frameNorm.dispose()
 
     self.postMessage(data)
+
+    busy = false
 })
 
